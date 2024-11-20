f0ff81de2cbcb860f5279617d0223a258a127b9d
theomerman <edsonanime@hotmail.es>
2024-11-19 19:51:49 -0600

    chore: added configuration for prisma

--- apps/server/src/index.ts
+++ apps/server/src/index.ts
@@ -1,19 +1,27 @@
 import bodyParser from 'body-parser'
 import express from 'express'
 import cors from 'cors'
+import { PrismaClient } from '@repo/db'
 
 const app = express()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cors())
+const prisma = new PrismaClient()
 
 app.get('/', (_, res) => {
-  return res.json({ ok: true })
+    prisma.user.create({
+        data: {
+            email: 'h',
+            name: 'hd',
+        },
+    })
+    return res.json({ ok: true })
 })
 
 const port = process.env.PORT || 5001
 
 app.listen(port, () => {
-  console.log(`Server API running on http://localhost:${port}`)
+    console.log(`Server API running on http://localhost:${port}`)
 })
